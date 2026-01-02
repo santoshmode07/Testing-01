7cacf9007a81d61703f893e76c30b397496e516e
santoshmode07 <santoshmode7@gmail.com>
2026-01-02 10:16:35 +0530

    Refactoring for MVC

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,18 +1,4 @@
-const fs=require('fs');
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is : ${val}`);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-}
+const Tour = require('./../models/tourModel');
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -29,100 +15,89 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    results: tours.length,
-    data: { tours },
+    // results: tours.length,
+    // data: { tours },
   });
 };
 
 exports.getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-
- 
-
-  res.status(200).json({
-    status: 'success',
-    data: { tour },
-  });
+  // console.log(req.params);
+  // const id = req.params.id * 1;
+  // const tour = tours.find((el) => el.id === id);
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: { tour },
+  // });
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      if (err) {
-        return res.status(500).json({
-          status: 'fail',
-          message: 'Error writing file',
-        });
-      }
-      res.status(201).json({
-        status: 'success',
-        data: { tour: newTour },
-      });
-    }
-  );
+  // const newId = tours[tours.length - 1].id + 1;
+  // const newTour = Object.assign({ id: newId }, req.body);
+  // tours.push(newTour);
+  // fs.writeFile(
+  //   `${__dirname}/../dev-data/data/tours-simple.json`,
+  //   JSON.stringify(tours),
+  //   (err) => {
+  //     if (err) {
+  //       return res.status(500).json({
+  //         status: 'fail',
+  //         message: 'Error writing file',
+  //       });
+  //     }
+  //     res.status(201).json({
+  //       status: 'success',
+  //       data: { tour: newTour },
+  //     });
+  //   },
+  // );
 };
 
 exports.updateTour = (req, res) => {
-  const id = req.params.id * 1;
-
+  // const id = req.params.id * 1;
   // Find the index of the tour to update
-  const tourIndex = tours.findIndex((el) => el.id === id);
-
-
+  // const tourIndex = tours.findIndex((el) => el.id === id);
   // Manually update the tour data by combining both the properties
-  const updatedTour = { ...tours[tourIndex], ...req.body };
-
+  // const updatedTour = { ...tours[tourIndex], ...req.body };
   // Replace the old tour with the updated one in the array
-  tours[tourIndex] = updatedTour;
-
+  // tours[tourIndex] = updatedTour;
   // Save the updated tours array to the file
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      if (err) {
-        return res.status(500).json({
-          status: 'fail',
-          message: 'Error writing file',
-        });
-      }
-
-      res.status(200).json({
-        status: 'success',
-        data: { tour: updatedTour },
-      });
-    }
-  );
+  // fs.writeFile(
+  //   `${__dirname}/dev-data/data/tours-simple.json`,
+  //   JSON.stringify(tours),
+  //   (err) => {
+  //     if (err) {
+  //       return res.status(500).json({
+  //         status: 'fail',
+  //         message: 'Error writing file',
+  //       });
+  //     }
+  //     res.status(200).json({
+  //       status: 'success',
+  //       data: { tour: updatedTour },
+  //     });
+  //   },
+  // );
 };
 
 exports.deleteTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tourIndex = tours.findIndex((el) => el.id === id);
+  // const id = req.params.id * 1;
+  // const tourIndex = tours.findIndex((el) => el.id === id);
 
-  tours.splice(tourIndex, 1);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      if (err) {
-        return res.status(500).json({
-          status: 'fail',
-          message: 'Error writing file',
-        });
-      }
-    }
-  );
+  // tours.splice(tourIndex, 1);
+  // fs.writeFile(
+  //   `${__dirname}/dev-data/data/tours-simple.json`,
+  //   JSON.stringify(tours),
+  //   (err) => {
+  //     if (err) {
+  //       return res.status(500).json({
+  //         status: 'fail',
+  //         message: 'Error writing file',
+  //       });
+  //     }
+  //   },
+  // );
   res.status(204).json({
     status: 'success',
     data: null,
   });
 };
-
--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -5,10 +5,17 @@ const fs = require('fs');
 
 const router = express.Router();
 
-router.param('id', tourController.checkID);
+// router.param('id', tourController.checkID);
 
-router.route('/').get(tourController.getAllTours).post(tourController.checkBody,tourController.createTour);
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.checkBody, tourController.createTour);
 
-router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
