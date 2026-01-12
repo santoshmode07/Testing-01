4ef256779b0cea068bf06ec8b7d56007a073692b
santoshmode07 <santoshmode7@gmail.com>
2026-01-12 21:36:05 +0530

    Adding the Query Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -55,6 +55,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     //Virtual Properties
@@ -83,6 +87,19 @@ tourSchema.pre('save', function (next) {
 //   console.log('Document saved:', doc);
 //   next();
 // });
+
+//QUERY MIDDLEWARE
+
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took $(Date.now()-this.start) ms`);
+  console.log(docs);
+  next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
