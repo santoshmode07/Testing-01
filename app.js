586754a137d24292d31b23df477a6980fc81542d
santoshmode07 <santoshmode7@gmail.com>
2026-01-25 16:47:36 +0530

    removed config.env from repo

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./Utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
