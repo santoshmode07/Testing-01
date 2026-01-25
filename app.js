cb22f5672543a460bad26176eaa9dea65b28ccd9
santoshmode07 <santoshmode7@gmail.com>
2026-01-25 17:39:12 +0530

    Implementing rate-limiting

--- app.js
+++ app.js
@@ -9,10 +9,19 @@ const userRouter = require('./routes/userRouter');
 
 const app = express();
 
-// 1)MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
+
 app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
